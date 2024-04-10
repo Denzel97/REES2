847f9c0d02f5d4026eb988a086b9981be3cc6df3
denzel97 <denzelwash97@gmail.com>
2024-04-10 19:57:04 +0300

    deployment to vercel

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 # build_files.sh
+python_path="C:\Users\Ailoe\AppData\Local\Programs\Python\Python312\Scripts\python.exe"
 python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
