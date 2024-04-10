bf5a02e3dc8c230002a327ceb0c70b4f58c9dd31
denzel97 <denzelwash97@gmail.com>
2024-04-10 19:39:34 +0300

    deployment to vercel

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-# Install pip if not available (requires sudo)
-# sudo apt update
-# sudo apt install python3-pip
-
-# Use Python provided by Vercel, which should already have pip
-python3 -m pip install -r requirements.txt
-
-# Make migrations and collect static files
-python3 manage.py collectstatic --no-input
+# build_files.sh
+python -m pip install -r requirements.txt
+python manage.py collectstatic --no-input
