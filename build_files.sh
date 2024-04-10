d4bb394ae70cd21436d4358642906c1bd74611ba
denzel97 <denzelwash97@gmail.com>
2024-04-10 20:11:25 +0300

    deployment to vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,3 @@
 # build_files.sh
-# shellcheck disable=SC2034
-python_path="C:\Users\Ailoe\AppData\Local\Programs\Python\Python312\Scripts\python.exe"
-
 "C:\Users\Ailoe\AppData\Local\Programs\Python\Python312\Scripts\python.exe" -m pip install -r requirements.txt
 "C:\Users\Ailoe\AppData\Local\Programs\Python\Python312\Scripts\python.exe" manage.py collectstatic --no-input
