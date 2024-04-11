070a834862327901fa7fe49e948507074e4d8b24
denzel97 <denzelwash97@gmail.com>
2024-04-11 15:19:26 +0300

    Deployment2

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 # build_files.sh
-pip install -r requirements.txt
-# make migrations
-python3.9 manage.py collectstatic
+.\venv\Scripts\activate  # Activate virtual environment
+python -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --no-input
